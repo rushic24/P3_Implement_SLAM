74d51b3674733dea77d708f8a316ebc52e10755d
Rushi <rushic24@gmail.com>
2019-11-04 07:39:02 +0530

    done

--- robot_class.py
+++ robot_class.py
@@ -1,30 +1,14 @@
-from math import *
-import random
+import random as rr
 
-
-### ------------------------------------- ###
-# Below, is the robot class
-#
-# This robot lives in 2D, x-y space, and its motion is
-# pointed in a random direction, initially.
-# It moves in a straight line until it comes close to a wall 
-# at which point it stops.
-#
-# For measurements, it  senses the x- and y-distance
-# to landmarks. This is different from range and bearing as
-# commonly studied in the literature, but this makes it much
-# easier to implement the essentials of SLAM without
-# cluttered math.
-#
+# the robot class
 class robot:
-    
+
     # --------
-    # init:
-    #   creates a robot with the specified parameters and initializes
+    # init: 
+    #   creates a robot with the specified parameters and initializes 
     #   the location (self.x, self.y) to the center of the world
     #
-    def __init__(self, world_size = 100.0, measurement_range = 30.0,
-                 motion_noise = 1.0, measurement_noise = 1.0):
+    def __init__(self, world_size = 100.0, measurement_range = 30.0,motion_noise = 1.0, measurement_noise = 1.0):
         self.measurement_noise = 0.0
         self.world_size = world_size
         self.measurement_range = measurement_range
@@ -34,29 +18,29 @@ class robot:
         self.measurement_noise = measurement_noise
         self.landmarks = []
         self.num_landmarks = 0
-    
-    
+
+
     # returns a positive, random float
     def rand(self):
-        return random.random() * 2.0 - 1.0
-    
-    
+        return rr.random() * 2.0 - 1.0
+
+
     # --------
     # move: attempts to move robot by dx, dy. If outside world
     #       boundary, then the move does nothing and instead returns failure
     #
     def move(self, dx, dy):
-        
+
         x = self.x + dx + self.rand() * self.motion_noise
         y = self.y + dy + self.rand() * self.motion_noise
-        
+
         if x < 0.0 or x > self.world_size or y < 0.0 or y > self.world_size:
             return False
         else:
             self.x = x
             self.y = y
             return True
-
+    
 
     # --------
     # sense: returns x- and y- distances to landmarks within visibility range
@@ -65,8 +49,7 @@ class robot:
     #        landmarks to be visible at all times
     #
     
-    ## TODO: paste your complete the sense function, here
-    ## make sure the indentation of the code is correct
+    ## TODO: complete the sense function
     def sense(self):
         ''' This function does not take in any parameters, instead it references internal variables
             (such as self.landamrks) to measure the distance between the robot and any landmarks
@@ -86,30 +69,40 @@ class robot:
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
         ##    - Feel free to use the function self.rand() to help calculate this noise component
+        ##    - It may help to reference the `move` function for noise calculation
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
         
         ## TODO: return the final, complete list of measurements
+        idx=0
+        for i in self.landmarks:
+            dx = abs(i[0]- self.x)
+            dy = abs(i[1]- self.y)
+            noise = self.rand()*self.measurement_noise
+            dx=dx+noise
+            dy=dy+noise
+            if dx > self.measurement_range or dy> self.measurement_range:
+                continue
+            else:
+                measurements.append([idx, dx, dy])
+            idx=idx+1
+            
         return measurements
 
-
+    
     # --------
-    # make_landmarks:
+    # make_landmarks: 
     # make random landmarks located in the world
     #
     def make_landmarks(self, num_landmarks):
         self.landmarks = []
         for i in range(num_landmarks):
-            self.landmarks.append([round(random.random() * self.world_size),
-                                   round(random.random() * self.world_size)])
+            self.landmarks.append([round(rr.random() * self.world_size),
+                                   round(rr.random() * self.world_size)])
         self.num_landmarks = num_landmarks
-
-
+    
+    
     # called when print(robot) is called; prints the robot's location
     def __repr__(self):
         return 'Robot: [x=%.5f y=%.5f]'  % (self.x, self.y)
-
-
-
-####### END robot class #######
